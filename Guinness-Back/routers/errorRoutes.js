94c1d6a0931c73a7797deb21250f69635042fe6e
waterjin <waterjin326@gmail.com>
2022-05-30 22:03:18 +0900

    remove all file

--- Guinness-Back/routers/errorRoutes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const errorController = require("../controllers/errorController");
-
-router.use(errorController.pageNotFoundError);
-router.use(errorController.internalServerError);
-
-module.exports = router;
--- README.md
+++
@@ -1,2 +0,0 @@
-# Guinness-Book-Web-Service
-이색 기네스북 웹 서비스
