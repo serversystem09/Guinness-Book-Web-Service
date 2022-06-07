aa99d3b5e45318784201019ddaf4a921d87444cd
서민영 <1ss1222@naver.com>
2022-06-07 14:42:53 +0900

    post crud, image

--- Guinness-Back/models/index.js
+++ Guinness-Back/models/index.js
@@ -13,6 +13,7 @@ let sequelize = new Sequelize(
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
+
 //모델과 연결
 db.User = require("./user.js")(sequelize, Sequelize);
 db.Post = require("./post.js")(sequelize, Sequelize);
@@ -32,6 +33,40 @@ db.Comment.belongsTo(db.Post);
 
 db.User.hasMany(db.Comment);
 db.Comment.belongsTo(db.User);
+
+
+
+
+
+
+
+// 이미지 받았을 때 필터링
+const imageFilter = (req, file, cb) => {
+  if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+    return cb(new Error("Only image files are allowed!"));
+  }
+  cb(null, true);
+};
+
+var storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    // 서버에 저장될 위치
+    cb(null, __basedir + "/app/static/assets/");
+  },
+  filename: (req, file, cb) => {
+    // 서버에 저장될 때 파일 이름
+    cb(null, `${Date.now()}-bezkoder-${file.originalname}`);
+  }
+});
+
+var uploadFile = multer({ storage: storage, fileFilter: imageFilter }).single(
+  // 프론트에서 넘겨울 params key 값, 오른쪽 같이 넘겨줘야함-> {photo: binary}
+  "photo"
+);
+module.exports = uploadFile;
+
+
+
 
 //팔로잉-팔로워 다대다 관계이므로 중간(관계) 테이블(Follow) 생성됨
 db.User.belongsToMany(db.User, {
