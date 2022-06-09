dbc5c17ab5f313e25f48a2f822ca82369460e379
waterjin <waterjin326@gmail.com>
2022-06-09 20:38:44 +0900

    signUp-api + login-api

--- Guinness-Back/Validation.js
+++ Guinness-Back/Validation.js
@@ -5,3 +5,9 @@ exports.signupValidation = [
     check('email', 'Please include a valid email').isEmail().normalizeEmail({ gmail_remove_dots: true }),
     check('password', 'Password must be 6 or more characters').isLength({ min: 6 })
 ]
+
+exports.loginValidation = [
+    check('email', 'Please include a valid email').isEmail().normalizeEmail({ gmail_remove_dots: true }),
+    check('password', 'Password must be 6 or more characters').isLength({ min: 6 })
+
+]
--- Guinness-Back/router.js
+++ Guinness-Back/router.js
@@ -1,16 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const db  = require('./dbConnection');
-const { signupValidation } = require('./validation');
+const { signupValidation, loginValidation } = require('./validation');
 const { validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-const postlist = ["post1", "post2"];
 
-router.get("/postlist", (req, res) => {
-  res.send(postlist);
-});
 router.post('/register', signupValidation, (req, res, next) => {
   db.query(
     `SELECT * FROM user WHERE LOWER(email) = LOWER(${db.escape(
@@ -52,6 +48,53 @@ router.post('/register', signupValidation, (req, res, next) => {
     }
   );
 });
- 
+
+router.post('/login', loginValidation, (req, res, next) => {
+  db.query(
+    `SELECT * FROM user WHERE email = ${db.escape(req.body.email)};`,
+    (err, result) => {
+      // user does not exists
+      if (err) {
+        throw err;
+        return res.status(400).send({
+          msg: err
+        });
+      }
+      if (!result.length) {
+        return res.status(401).send({
+          msg: 'Email or password is incorrect!'
+        });
+      }
+      // check password
+      bcrypt.compare(
+        req.body.password,
+        result[0]['password'],
+        (bErr, bResult) => {
+          // wrong password
+          if (bErr) {
+            throw bErr;
+            return res.status(401).send({
+              msg: 'Email or password is incorrect!'
+            });
+          }
+          if (bResult) {
+            const token = jwt.sign({id:result[0].id},'the-super-strong-secrect',{ expiresIn: '1h' });
+            /*db.query(
+              `UPDATE user SET last_login = now() WHERE id = '${result[0].id}'`
+            );*/
+            return res.status(200).send({
+              msg: 'Logged in!',
+              token,
+              user: result[0]
+            });
+          }
+          return res.status(401).send({
+            msg: 'Username or password is incorrect!'
+          });
+        }
+      );
+    }
+  );
+});
  
 module.exports = router;
