ca2929fd84a1ed011593b36989d0909984e5ddb8
서민영 <1ss1222@naver.com>
2022-06-13 01:00:47 +0900

    rank 완료

--- Guinness-Back/controllers/indexController.js
+++ Guinness-Back/controllers/indexController.js
@@ -1,35 +1,22 @@
-import {getHotRanks} from "../models/ranks.js";
+import {getByLikes, getByUserAmount} from "../models/ranks.js";
 
-
-/*
 //main에서 볼 수 있는 등수 항목들
 export const showHots = (req, res) => {
     getByLikes((err, results) => {
         if (err){
             res.send(err);
         }else{
-            res.json(results);
-
- //           export const showHotCategory = (req, res) => {
-                getByUserAmount((err, results) => {
-                    if (err){
-                        res.send(err);
-                    }else{
-                        res.json(results);
-                    }
-                });
-            }            
+            res.json(results);          
         }
-    );
+     });
 }
-*/
 
-export const showHotRanks = (req, res) => {
-    getHotRanks((err, results) => {
+export const showHotCategory = (req, res) => {
+    getByUserAmount((err, results) => {
         if (err){
             res.send(err);
         }else{
             res.json(results);
-        } 
+        }
     });
-}
+}  
