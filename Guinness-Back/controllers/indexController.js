d87c81cc0c32a5f0ef77165d5a6c3574ca556076
서민영 <1ss1222@naver.com>
2022-06-12 12:57:22 +0900

    ranking 수정 2

--- Guinness-Back/controllers/indexController.js
+++ Guinness-Back/controllers/indexController.js
@@ -1,12 +1,13 @@
 import {getByUserAmount, getRankingByLikes} from "../models/rank.js";
 
 //main에서 볼 수 있는 등수 항목들
-export const showHots = (req, res) => {
+export const showHots = (req, res, next) => {
     getByLikes((err, results) => {
         if (err){
             res.send(err);
         }else{
             res.json(results);
+            next();
         }
     });
 }
--- Guinness-Back/routers/indexRoutes.js
+++ Guinness-Back/routers/indexRoutes.js
@@ -8,8 +8,8 @@ import { showHots, showHotCategory } from "../controllers/indexController.js";
 const indexRouter = express.Router();
 
 //2. event or category 참여자 수 기준 -> 게시글 4개 추출(인기 종목 순위) => 순위, 이벤트명
-indexRouter.get('/1', showHots);
-indexRouter.get('/2', showHotCategory);
+indexRouter.get('/', showHots, showHotCategory);
+//indexRouter.get('/2', showHotCategory);
 
 // export default router
 export default indexRouter;
