59a2d96524fdddc4f9d1dcd2b5417dbe636b0830
sujin <64936396+Waterjin6@users.noreply.github.com>
2022-06-12 09:26:16 +0900

    fix lock error in DB & alter user tbl

--- popete.sql
+++ popete.sql
@@ -1,29 +1,18 @@
 -- MySQL dump 10.13  Distrib 8.0.29, for Linux (x86_64)
 --
--- Host: localhost    Database: popete_db
+-- Host: localhost    Database: popeteDB
 -- ------------------------------------------------------
 -- Server version	8.0.29-0ubuntu0.20.04.3
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `attachment`
 --
-DROP database IF EXISTS `popete_db`;
-CREATE database popete_db;
-use popete_db;
+DROP database IF EXISTS `popeteDB`;
+CREATE database popeteDB;
+use popeteDB;
 
 DROP TABLE IF EXISTS `attachment`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `attachment` (
   `postNum` int NOT NULL,
   `fileNum` int NOT NULL,
@@ -33,54 +22,36 @@ CREATE TABLE `attachment` (
   `size` int DEFAULT NULL,
   `saveOrder` int DEFAULT NULL,
   PRIMARY KEY (`postNum`,`fileNum`),
-  CONSTRAINT `attachment_ibfk_1` FOREIGN KEY (`postNum`) REFERENCES `post` (`postNum`),
-  CONSTRAINT `attachment_ibfk_2` FOREIGN KEY (`postNum`) REFERENCES `post` (`postNum`),
-  CONSTRAINT `attachment_ibfk_3` FOREIGN KEY (`postNum`) REFERENCES `post` (`postNum`),
-  CONSTRAINT `FK_PostAttached` FOREIGN KEY (`postNum`) REFERENCES `post` (`postNum`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  CONSTRAINT `attachment_ibfk_1` FOREIGN KEY (`postNum`) REFERENCES `post` (`postNum`) ON DELETE CASCADE
+);
 
 --
 -- Dumping data for table `attachment`
 --
 
-LOCK TABLES `attachment` WRITE;
-/*!40000 ALTER TABLE `attachment` DISABLE KEYS */;
+
 INSERT INTO `attachment` VALUES (1011,1,'gag','/c/w','.jpg',4,1);
-/*!40000 ALTER TABLE `attachment` ENABLE KEYS */;
-UNLOCK TABLES;
+
 
 --
 -- Table structure for table `category`
 --
 
 DROP TABLE IF EXISTS `category`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `category` (
   `categoryNum` int NOT NULL,
   `categoryName` varchar(10) DEFAULT NULL,
   PRIMARY KEY (`categoryNum`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+);
 
---
--- Dumping data for table `category`
---
-
-LOCK TABLES `category` WRITE;
-/*!40000 ALTER TABLE `category` DISABLE KEYS */;
 INSERT INTO `category` VALUES (1,'운동');
-/*!40000 ALTER TABLE `category` ENABLE KEYS */;
-UNLOCK TABLES;
+
 
 --
 -- Table structure for table `comment`
 --
 
 DROP TABLE IF EXISTS `comment`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `comment` (
   `commentNum` int NOT NULL,
   `postNum` int DEFAULT NULL,
@@ -93,29 +64,24 @@ CREATE TABLE `comment` (
   KEY `postNum` (`postNum`),
   KEY `eventID` (`eventID`),
   KEY `userID` (`userID`),
-  CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`postNum`) REFERENCES `post` (`postNum`),
+  CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`postNum`) REFERENCES `post` (`postNum`) ON DELETE CASCADE,
   CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`eventID`) REFERENCES `event` (`eventID`),
   CONSTRAINT `comment_ibfk_3` FOREIGN KEY (`userID`) REFERENCES `user` (`userID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ;
 
 --
 -- Dumping data for table `comment`
 --
 
-LOCK TABLES `comment` WRITE;
-/*!40000 ALTER TABLE `comment` DISABLE KEYS */;
 INSERT INTO `comment` VALUES (1,1011,3000,'2022-05-28 11:01:47',111,'1111','ㅋㅋㅋ우리 할아버지도 50개는 함ㅋ');
-/*!40000 ALTER TABLE `comment` ENABLE KEYS */;
-UNLOCK TABLES;
+
 
 --
 -- Table structure for table `event`
 --
 
 DROP TABLE IF EXISTS `event`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `event` (
   `eventID` int NOT NULL,
   `eventName` varchar(20) DEFAULT NULL,
@@ -129,50 +95,41 @@ CREATE TABLE `event` (
   KEY `categoryNum` (`categoryNum`),
   CONSTRAINT `event_ibfk_1` FOREIGN KEY (`createrID`) REFERENCES `user` (`userID`),
   CONSTRAINT `event_ibfk_2` FOREIGN KEY (`categoryNum`) REFERENCES `category` (`categoryNum`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+);
 
 --
 -- Dumping data for table `event`
 --
 
-LOCK TABLES `event` WRITE;
-/*!40000 ALTER TABLE `event` DISABLE KEYS */;
+
 INSERT INTO `event` VALUES (3000,'팔굽혀펴기 한번에 몇 개 까지',1,100,'팔굽혀펴기 한번에 몇 개 까지 가능하세요?','2022-05-27 04:51:15','20200975');
-/*!40000 ALTER TABLE `event` ENABLE KEYS */;
-UNLOCK TABLES;
+
 
 --
 -- Table structure for table `follow`
 --
 
 DROP TABLE IF EXISTS `follow`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `follow` (
   `followerID` varchar(20) NOT NULL,
   `followeeID` varchar(20) NOT NULL,
   PRIMARY KEY (`followerID`,`followeeID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ;
 
 --
 -- Dumping data for table `follow`
 --
 
-LOCK TABLES `follow` WRITE;
-/*!40000 ALTER TABLE `follow` DISABLE KEYS */;
+
 INSERT INTO `follow` VALUES ('20200975','1111');
-/*!40000 ALTER TABLE `follow` ENABLE KEYS */;
-UNLOCK TABLES;
+
 
 --
 -- Table structure for table `post`
 --
 
 DROP TABLE IF EXISTS `post`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `post` (
   `postNum` int NOT NULL,
   `postTitle` varchar(40) DEFAULT NULL,
@@ -187,53 +144,33 @@ CREATE TABLE `post` (
   KEY `writerID` (`writerID`),
   CONSTRAINT `post_ibfk_1` FOREIGN KEY (`eventID`) REFERENCES `event` (`eventID`),
   CONSTRAINT `post_ibfk_2` FOREIGN KEY (`writerID`) REFERENCES `user` (`userID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+);
 
---
--- Dumping data for table `post`
---
 
-LOCK TABLES `post` WRITE;
-/*!40000 ALTER TABLE `post` DISABLE KEYS */;
 INSERT INTO `post` VALUES (1011,'한번에 50개 완전 가능이지','쉽쥬?',3000,'20200975',123,0,'2022-05-27 09:10:52');
-/*!40000 ALTER TABLE `post` ENABLE KEYS */;
-UNLOCK TABLES;
+
 
 --
 -- Table structure for table `recommend`
 --
 
 DROP TABLE IF EXISTS `recommend`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `recommend` (
   `recommendNum` int NOT NULL,
   `eventID` int NOT NULL,
   `postNum` int DEFAULT NULL,
   PRIMARY KEY (`recommendNum`,`eventID`),
   KEY `postNum` (`postNum`),
-  CONSTRAINT `recommend_ibfk_1` FOREIGN KEY (`postNum`) REFERENCES `post` (`postNum`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  CONSTRAINT `recommend_ibfk_1` FOREIGN KEY (`postNum`) REFERENCES `post` (`postNum`) ON DELETE CASCADE
+);
 
---
--- Dumping data for table `recommend`
---
 
-LOCK TABLES `recommend` WRITE;
-/*!40000 ALTER TABLE `recommend` DISABLE KEYS */;
+
+
 INSERT INTO `recommend` VALUES (1010,3000,1011);
-/*!40000 ALTER TABLE `recommend` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `user`
---
 
 DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
   `userID` int NOT NULL AUTO_INCREMENT,
   `nickName` varchar(50) DEFAULT NULL,
@@ -242,27 +179,9 @@ CREATE TABLE `user` (
   `email` varchar(50) NOT NULL UNIQUE,
   `ranking` varchar(10) DEFAULT NULL,
   `birth` datetime DEFAULT NULL,
+  `last_login` datetime DEFAULT '2001-03-26',
   PRIMARY KEY (`userID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+);
 
---
--- Dumping data for table `user`
---
 
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
 INSERT INTO `user` VALUES ('1111','johnDoe','johnho',01044444444,'johnDoe@gmail.com','gold','2001-04-04 00:00:00'),('20200975','waterjin','starstar',01082636230,'20200975@sungshin.ac.kr','baby','2001-03-26 00:00:00');
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2022-06-03 11:32:52
