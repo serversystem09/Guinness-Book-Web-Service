ce6a82aa7c0d0b659de75ae43e86e89c289840a4
서민영 <1ss1222@naver.com>
2022-06-14 12:56:16 +0900

    rank 관련 수정 사항

--- Guinness-Back/models/ranks.js
+++ Guinness-Back/models/ranks.js
@@ -1,21 +1,21 @@
 // import connection
 import db from "../config/dbConnection.js";
 
-// //1. /main/2
-// //좋아요 기준 -> 게시글 3개 추출(hot 게시글) => 사진, 좋아요, 사용자명 카드로
-// export const getByLikes = (result) => {
-//   db.query(
-//     "SELECT user.nickName, post.postTitle, post.photo, post.likeNum as likes     from post    join user    on  post.writerID = user.userID     GROUP BY writerID     ORDER BY likes DESC",
-//     (err, results) => {
-//       if (err) {
-//         console.log(err);
-//         result(err, null);
-//       } else {
-//         result(null, results);
-//       }
-//     }
-//   );
-// };
+ //1. /main/1
+ //좋아요 기준 -> 게시글 3개 추출(hot 게시글) => 사진, 좋아요, 사용자명 카드로
+ export const getByLikes = (result) => {
+   db.query(
+     "SELECT user.nickName, post.postTitle, post.photo, post.likeNum as likes     from post    join user    on  post.writerID = user.userID     GROUP BY writerID     ORDER BY likes DESC",
+     (err, results) => {
+        if (err) {
+         console.log(err);
+         result(err, null);
+       } else {
+         result(null, results);
+       }
+     }
+   ); 
+ };
 
 //2. /main/2
 //category 참여자 수 기준 -> 게시글 4개 추출 => 순위, 참여자 수, category 명
